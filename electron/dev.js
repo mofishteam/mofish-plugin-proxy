c23623bc9f6142eb2ccd138d11f7599e026bbc0e
wszxdhr <wszxdhr@126.com>
2020-02-28 20:26:38 +0800

    feat:调试electron

---
+++ electron/dev.js
@@ -0,0 +1,18 @@
+// Install `electron-debug` with `devtron`
+require('electron-debug')({ showDevTools: true })
+
+// Install `vue-devtools`
+require('electron').app.on('ready', () => {
+  let installExtension = require('electron-devtools-installer')
+  installExtension.default(installExtension.VUEJS_DEVTOOLS)
+    .then(() => {})
+    .catch(err => {
+      console.log('Unable to install `vue-devtools`: \n', err)
+    })
+})
+
+const vueConfig = require('../vue.config')
+global.vuePort = vueConfig.devServer.port
+global.isDev = true
+// Require `main` process to boot app
+require('./index.js')
---
+++ electron/index.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+require('../dist-server/index.js')
+
+function createWindow () {
+  // 创建浏览器窗口
+  let win = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    titleBarStyle: 'hidden',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // 加载index.html文件
+  if (global.vuePort) {
+    win.loadURL(`http://localhost:${global.vuePort}`)
+  } else {
+    win.loadFile('index.html')
+  }
+}
+
+app.on('ready', createWindow)
