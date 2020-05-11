ebb9bb03406771ae7192963664b3c7d8f3a21afc
wszxdhr <wszxdhr@126.com>
2020-05-11 17:06:08 +0800

    feat(tool):引入mofish-common-tool

--- electron/index.js
+++ electron/index.js
@@ -1,6 +1,9 @@
 const { app, BrowserWindow } = require('electron')
 require('@babel/polyfill')
 require('../dist-server/proxy.js')
+const commonTool = require('mofish-common-tool')
+
+console.log(commonTool)
 
 function createWindow () {
   // 创建浏览器窗口
