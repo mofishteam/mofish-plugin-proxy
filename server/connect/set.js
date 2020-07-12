a7cf71d479f19dbcac15b610a6efe5278f0d1e98
wszxdhr <job@anymelon.com>
2020-07-12 23:15:19 +0800

    feat(ServerConnect):增加返回值

--- server/connect/set.js
+++ server/connect/set.js
@@ -2,6 +2,7 @@ export default async function ({ core, params, reply }) {
   switch (params.name) {
     case 'server': core.mergeServerConfig(params.id, params.change)
       await core.saveConfig()
+      reply({ type: 'success', data: core.getServer(params.id).config })
       break
   }
 }
